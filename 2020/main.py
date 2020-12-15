0ac6a718568242bd895945b71cc3961c98a484c7
Braden Watkins <brwatkin@microsoft.com>
2020-12-14 16:57:50 -0800

    Add day 5 and 6

--- 2020/main.py
+++ 2020/main.py
@@ -1,5 +1,7 @@
+import sys
 from datetime import date
 import runner
 
 today = (date.today() - date(2020, 12, 1)).days + 1
-runner.run(day=today)
+day = sys.argv[1] if len(sys.argv) > 1 else today
+runner.run(day=day)
